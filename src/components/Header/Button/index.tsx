17f1c050bbdeacbbbf68824da9377a60756725c2
saur0w <sthapliyal085@gmail.com>
2026-07-12 22:27:21 +0530

    feat: implement reusable Header component with navigation button

---
+++ src/components/Header/Button/index.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import styles from "./style.module.scss";
+
+export default function Button() {
+    return (
+        <div className={styles.button}>
+            <p>Menu</p>
+        </div>
+    )
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import styles from "./style.module.scss";
+import Button from "./Button";
 
 export default function Header() {
     return (
         <header className={styles.header}>
-            
+            <Button />
         </header>
     )
 }
