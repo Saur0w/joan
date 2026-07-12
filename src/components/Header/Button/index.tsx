a199c2bd72ba37f630cc2a1ae5a2696cc4d39fb7
saur0w <sthapliyal085@gmail.com>
2026-07-12 23:26:37 +0530

    feat: implement Header component with animated toggle button using GSAP

--- src/components/Header/Button/index.tsx
+++ src/components/Header/Button/index.tsx
@@ -1,15 +1,36 @@
 "use client";
 
 import styles from "./style.module.scss";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
 
-export default function Button() {
+gsap.registerPlugin(useGSAP);
+
+interface ButtonProps {
+    isActive: boolean;
+    setIsActive: (isActive: boolean) => void;
+}
+
+export default function Button({isActive, setIsActive}: ButtonProps) {
+    const buttonRef = useRef<HTMLDivElement>(null);
+    const sliderRef = useRef<HTMLDivElement>(null);
+    useGSAP(() => {
+        gsap.to(sliderRef.current, {
+            top: isActive ? "-100%" : "0%",
+            duration: 0.5,
+            ease: "power3.inOut",
+        });
+    }, { scope: buttonRef, dependencies: [isActive]})
     return (
-        <div className={styles.button}>
-            <div className={styles.el}>
-                <p>Menu</p>
-            </div>
-            <div className={styles.el}>
-                <p>Close</p>
+        <div onClick={() => { setIsActive(!isActive)}} className={styles.button} ref={buttonRef}>
+            <div className={styles.slider} ref={sliderRef}>
+                <div className={styles.el}>
+                    <p>Menu</p>
+                </div>
+                <div className={styles.el}>
+                    <p>Close</p>
+                </div>
             </div>
         </div>
     )
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,11 +2,13 @@
 
 import styles from "./style.module.scss";
 import Button from "./Button";
+import {useState} from "react";
 
 export default function Header() {
+    const [isActive, setIsActive] = useState(false);
     return (
         <header className={styles.header}>
-            <Button />
+            <Button isActive={isActive} setIsActive={setIsActive} />
         </header>
     )
 }
