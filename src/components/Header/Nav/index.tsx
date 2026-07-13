a6e4b00e4a8514447bd9a5fbf9374dd140d33f92
saur0w <sthapliyal085@gmail.com>
2026-07-13 12:59:44 +0530

    feat: add navigation component with GSAP animation integration

--- src/components/Header/Nav/index.tsx
+++ src/components/Header/Nav/index.tsx
@@ -3,16 +3,31 @@
 import styles from "./style.module.scss";
 import { Links } from "./data";
 import Link from "next/link";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from"react";
+
+gsap.registerPlugin(useGSAP);
 
 export default function Nav() {
+    const navRef = useRef<HTMLDivElement>(null);
+    const textRef = useRef<HTMLDivElement>(null);
+
+    useGSAP(() => {
+        gsap.to(textRef.current, {
+            opacity: 0
+        })
+    }, { scope: navRef });
     return (
-        <div className={styles.nav}>
+        <div className={styles.nav} ref={navRef}>
             <div className={styles.body}>
                 {
                     Links.map((link, i) => {
-                        return <div key={i}>
-                            <Link href={link.href}>{link.title}</Link>
+                        return (
+                            <div key={i}>
+                                <Link href={link.href}>{link.title}</Link>
                             </div>
+                        )
                     })
                 }
             </div>
